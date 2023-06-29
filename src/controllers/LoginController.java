a92015d34d57ad67b9af5830509ee6fa207da801
klight <phamminhquang2k2@gmail.com>
2023-06-29 10:24:40 +0700

    add check login

---
+++ src/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.sql.*;
+
+import models.UserEntity;
+import models.db.DB;
+
+public class LoginController {
+    public static UserEntity currentUser = new UserEntity();
+	 public boolean login(String userName, String password) throws SQLException, ClassNotFoundException{
+	        Connection connection = DB.getConnection();
+	        Statement st = connection.createStatement();
+	        ResultSet rs = st.executeQuery("SELECT * FROM users WHERE userName = '" + userName +"'");
+	        if (rs == null) {
+	            return false;
+	        }
+	        while (rs.next()) {                
+	            if (rs.getString("password") == null ? password == null : rs.getString("password").equals(password)) {
+	                LoginController.currentUser.setID(rs.getInt("id"));
+	                LoginController.currentUser.setUserName(rs.getString("userName"));
+	                return true;
+	            }
+	        }
+	        connection.close();
+	        return false;
+	    }
+}
