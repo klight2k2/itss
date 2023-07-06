bf8f187347e0ae5d1fce8795fa4af53a6e820853
klight <phamminhquang2k2@gmail.com>
2023-07-06 18:53:05 +0700

    add login

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -117,4 +117,21 @@ public class UserService {
 			return false;
 		}
 	}
+	 public boolean checkLogin(String userName, String password) throws SQLException, ClassNotFoundException{
+	        Connection connection = DB.getConnection();
+	        Statement st = connection.createStatement();
+	        ResultSet rs = st.executeQuery("SELECT * FROM users WHERE userName = '" + userName +"'");
+	        if (rs == null) {
+	            return false;
+	        }
+	        while (rs.next()) {                
+	            if (rs.getString("password") == null ? password == null : rs.getString("password").equals(password)) {
+	                LoginController.currentUser.setId(rs.getInt("id"));
+	                LoginController.currentUser.setName(rs.getString("userName"));
+	                return true;
+	            }
+	        }
+	        connection.close();
+	        return false;
+	    }
 }
